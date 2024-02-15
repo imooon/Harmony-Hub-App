5ad22290d4f0618d8e6a5b347585563d247e674c
daisyisibor <daisyisibor8@gmail.com>
2024-02-14 22:22:51 -0500

    added click function for sound and video page

--- assets/main.js
+++ assets/main.js
@@ -5,3 +5,14 @@ function videoPage(){
     window.location.href="video.html";
 }
 
+function quizPage(){
+    window.location.href="quiz.html";
+}
+
+function informationPage(){
+    window.location.href="info.html";
+}
+
+function positivePage(){
+    window.location.href="positive.html";
+}
