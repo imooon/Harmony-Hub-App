a1a71bf3d55183af931a287b7c60963432000b27
Sheryl L <s_lawrence0@yahoo.ca>
2024-02-17 15:44:20 -0500

    js work for button

--- assets/positive.js
+++ assets/positive.js
@@ -1,8 +1,9 @@
-
-//   document.getElementById('button').addEventListener('click", function() {
-//   var clickForPicture = 
-//   document.getElementById('picture');picture.src = './images/dogwithballimage.jpeg';
-//   });
+//    document.getElementById('#button').addEventListener('click", function() {
+//    var picture =
+//    document.getElementById('#picture');
+//    picture.src = './images/dogwithballimage.jpeg';
+//    picture.style.display = 'block';
+//    });
 
 // var button = document.querySelector("#button");
 // var container = document.querySelector("#container");
@@ -12,4 +13,17 @@
 // function() {
 //   if (clicked === "picture")
 //   container.setAttribute(src = "./images/dogwithball")
+// });
+
+// // var count = 0;
+// var button = document.querySelector("#button");
+// var imgEl = document.querySelector("#picture");
+
+// function showPicture() {
+//   imgEl.setAttribute("src"= images/dogwithballimage.jpeg);
+// )}
+// // Attach event listener to increment button element
+// imgEl.addEventListener("click", function() {
+// //   count++;
+//   showPicture();
 // });
