2ce743b8a2d94ec14e215d6d60357d2e044d4ca5
daisyisibor <daisyisibor8@gmail.com>
2024-02-17 12:02:33 -0500

    added drop down menu for the sounds/ styled on css and created the function for display button on js included the spotify URL in js

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,67 @@
+// this is a function for when the display button ic clicked what would be displayed
+function display() {
+    var selectOptions= document.getElementById("selectOptions");
+    var selectedOption=selectOptions.options[selectOptions.selectedIndex].value;
+  //this encode the selected option for URL
+    var searchQuery = encodeURIComponent(selectedOption);
+
+// The Spotify API endpoint search
+  var apiUrl= "https://api.spotify.com/v1/search?type=album&q="  + searchQuery;
+
+
+  fetch(apiUrl)
+   .then(function(response){
+    return response.json();
+   })
+   .then(function(date){
+    displayResults(date);
+   })
+   .catch(function(error){
+    console.error('Error',error);
+   });
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function soundPage(){
     window.location.href="sound.html";
 }
