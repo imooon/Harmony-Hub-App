48837d5e80ca84d2e652ee19ee283cf75af33b47
Sheryl L <s_lawrence0@yahoo.ca>
2024-02-17 14:57:51 -0500

    js code work

--- assets/positive.js
+++ assets/positive.js
@@ -1,14 +1,15 @@
-// var click = document.querySelector()
 
-var onClick = document.querySelector()
-
-// function clickForPicture
-
-// onClick.addEventListener("click", function() {
-//     count++;
-//     setCounterText();
+//   document.getElementById('button').addEventListener('click", function() {
+//   var clickForPicture = 
+//   document.getElementById('picture');picture.src = './images/dogwithballimage.jpeg';
 //   });
 
-  document.getElementById('button').addEventListener('click", function() {
-  document.getElementById('picture').src = './images/dogwithballimage.jpeg';
-  });
+// var button = document.querySelector("#button");
+// var container = document.querySelector("#container");
+
+// var clicked = "picture";
+// button.addEventListener("click",
+// function() {
+//   if (clicked === "picture")
+//   container.setAttribute(src = "./images/dogwithball")
+// });
