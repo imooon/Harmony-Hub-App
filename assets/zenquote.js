fc7e75c61f206bb29225694ecb30bd8b75d27d93
Sheryl L <s_lawrence0@yahoo.ca>
2024-02-19 19:30:36 -0500

    overall edits

--- assets/zenquote.js
+++ assets/zenquote.js
@@ -3,19 +3,32 @@ var quoteTags = document.getElementById('quote-tags');
 var quoteAuthor = document.getElementById('quote-author');
 var genQuoteBtn = document.getElementById('generate-quote-button');
 
+function randomQuote();
 fetch("https://zenquotes.io/api/quotes/")
-    .then
-
-
-
-
-const api_url ="https://zenquotes.io/api/quotes/";
-
-async function getapi(url)
-{
-  const response = await fetch(url);
-  var data = await response.json();
-  console.log(data);
+    .then(response)=> response.json())
+    .then(data => (
+        quoteText.textContent = data.content;
+        quoteTags.textContent = data.tags;
+        quoteAuthor.textContent = `--${data.author};
+    });
 }
 
-getapi(api_url);
+randomQuote();
+genQuoteBtn.addEventListener('click', () => {
+    randomQuote():
+}),
+
+
+
+
+
+// const api_url ="https://zenquotes.io/api/quotes/";
+
+// async function getapi(url)
+// {
+//   const response = await fetch(url);
+//   var data = await response.json();
+//   console.log(data);
+// }
+
+// getapi(api_url);
